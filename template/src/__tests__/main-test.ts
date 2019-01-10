84a955f8baa42c12b8f3aec8f4ded9aee914981f
Brian Jacobel <bjacobel@gmail.com>
2019-01-09 21:31:20 -0500

    Name the main function... main

--- template/src/__tests__/main-test.ts
+++ template/src/__tests__/main-test.ts
@@ -1,29 +1,29 @@
-import SLAKPROJ from "../SLAKPROJ";
+import main from "../main";
 import log from "../utils/log";
 
 jest.mock("../utils/log");
 
-describe("SLAKPROJ function handler", () => {
+describe("main function handler", () => {
   describe("happy path of boilerplate", () => {
     it("resolves with details from event sent", () => {
-      expect(SLAKPROJ({ source: { service: "s3" } })).resolves.toEqual("s3");
+      expect(main({ source: { service: "s3" } })).resolves.toEqual("s3");
     });
   });
   describe("error handling", () => {
     it("logs the event with INFO level", () => {
-      return SLAKPROJ("nope").catch(() =>
+      return main("nope").catch(() =>
         expect(log.info).toHaveBeenCalledWith("nope")
       );
     });
 
     it("logs the error with ERROR level", async () => {
-      return SLAKPROJ("nope").catch(() =>
+      return main("nope").catch(() =>
         expect(log.error).toHaveBeenCalledWith(expect.any(TypeError))
       );
     });
 
     it("returns a promise rejection", () => {
-      return SLAKPROJ("nope").catch(err => expect(err).toBeInstanceOf(TypeError));
+      return main("nope").catch(err => expect(err).toBeInstanceOf(TypeError));
     });
   })
 });
